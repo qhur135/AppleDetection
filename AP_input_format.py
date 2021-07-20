d3e44026763dd6aebe6ef2f65d7f038e5ea44d5c
qhur135 <67954861+qhur135@users.noreply.github.com>
2021-07-20 18:05:05 +0900

    feat: to AP_input format

---
+++ AP_input_format.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import os
+
+for file in os.listdir('./result'):
+    new_csv = []
+    with open(f'./result/{file}') as f:
+        for index, line in enumerate(f.readlines()):
+            if index == 0:
+                continue
+            line = line.strip()
+            tmp = line.split(' ')
+            for idx in [2, 3, 4, 5]:
+                tmp[idx] = str(round(float(tmp[idx])))
+            new_csv.append(' '.join(tmp))
+    new_csv = pd.DataFrame(np.array(new_csv))
+    new_csv.to_csv(f'./result_tmp/{file}', header=False, index=False)
--- yolo_predict.py
+++ yolo_predict.py
@@ -5,6 +5,7 @@ import glob
 import numpy as np
 import os
 import cv2
+import pandas as pd
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5x')
@@ -25,27 +26,13 @@ for fname in os.listdir(ROOT_PATH):
 
     # Results
     results.print()
-    results.show()  # or .show()
+    # results.show()  # or .show()
+    # results.save()
 
-    save_file = './yolov-x.txt'
+    save_file = './yolo-x.csv'
 
-    with open(save_file,'a') as f:
-        f.write(results)
-    # results.xyxy[0]  # img1 predictions (tensor)
-    # results.pandas().xyxy[0]  # img1 predictions (pandas)
-    #
-    # if (results.pandas().xyxy[0][results.pandas().xyxy[0]['name'].str.contains('apple')]):
-    #
-    #     point = []
-    #
-    #     for i in range(len(results.pandas().xyxy[0])):
-    #         x1 = round(results.pandas().xyxy[0].xmax)
-    #         y1 = round(results.pandas().xyxy[0].ymax)
-    #         x2 = round(results.pandas().xyxy[0].xmin)
-    #         y2 = round(results.pandas().xyxy[0].ymin)
-    #
-    #         point.append([x1.values[i], y1.values[i], x2.values[i], y2.values[i]])
-    #
-    #         SAVE_FILE = './not_apple_gt_result.txt'
-    #         with open(SAVE_FILE, 'a') as f:
-    #             f.write(f'{fname},{point[i]}\n')
+    results = results.pandas().xyxy[0]  # img1 predictions (pandas)
+
+    results.to_csv(f'./result/{fname.split(".")[0]}.csv', sep=' ', float_format="%.6f",
+                   columns=["name", "confidence", "ymin", "xmin", "ymax", "xmax"], index=False)
+
